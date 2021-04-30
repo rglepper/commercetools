148049c2a8868d3921f20f72cafeb6af5c530113
andrzejewsky <vox3r69@gmail.com>
2021-04-30 14:46:24 +0200

    setup ts and lerna

---
+++ packages/api-client/src/index.ts
@@ -0,0 +1,5 @@
+export * from './fragments';
+export * from './types/Api';
+export * from './types/GraphQL';
+export * from './types/setup';
+export * as cartActions from './helpers/cart/actions';
---
+++ packages/rollup.base.config.js
@@ -0,0 +1,66 @@
+import typescript from 'rollup-plugin-typescript2';
+import graphqlPlugin from 'rollup-plugin-graphql';
+
+const defaults = {
+  graphql: false
+}
+
+export function generateBaseConfig(pkg, { graphql } = defaults) {
+  const mainEntrypoint = {
+    input: 'src/index.ts',
+    output: [
+      {
+        file: pkg.main,
+        format: 'cjs',
+        sourcemap: true
+      },
+      {
+        file: pkg.module,
+        format: 'es',
+        sourcemap: true
+      }
+    ],
+    external: [
+      ...Object.keys(pkg.dependencies || {}),
+      ...Object.keys(pkg.peerDependencies || {})
+    ],
+    plugins: [
+      typescript({
+        // eslint-disable-next-line global-require
+        typescript: require('typescript')
+      })
+    ]
+  };
+
+  const serverEntrypoint = {
+    input: 'src/index.server.ts',
+    output: [
+      {
+        file: pkg.server,
+        format: 'cjs',
+        sourcemap: true
+      }
+    ],
+    external: [
+      ...Object.keys(pkg.dependencies || {}),
+      ...Object.keys(pkg.peerDependencies || {})
+    ],
+    plugins: [
+      typescript({
+        // eslint-disable-next-line global-require
+        typescript: require('typescript')
+      })
+    ]
+  };
+
+  if (graphql) {
+    serverEntrypoint.plugins.push(graphqlPlugin())
+  }
+
+  if (pkg.server) {
+    return [mainEntrypoint, serverEntrypoint]
+  }
+
+
+  return mainEntrypoint;
+}
